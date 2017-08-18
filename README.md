8eaf7809906687fe9454ea39f0d4862e3b82029e
billy3321 <billy3321@gmail.com>
2017-08-18 22:35:05 +0800

    update content

--- README.md
+++
@@ -1,3 +0,0 @@
-# Akoma Ntoso Reader
-
-It's a akoma ntoso reader html, please check [here](https://jrf-tw.github.io/akn-reader/)
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  entry: './app/main.js',
-  output: {
-    path: __dirname + "/dist",
-    filename: 'bundle.js'
-  }
-};
